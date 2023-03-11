3854086a2cd5d297529a457eabbb4b6389b5ac9e
jasar <juanantonio.net@gmail.com>
2023-03-11 17:50:11 -0600

    server1

--- main.tf
+++ main.tf
@@ -1,14 +1,23 @@
 provider "aws" {
-  region = "aws"
-
+  region = "us-east-1"
+  
 }
+ resource "aws_instance" "Server1" {
+  ami = "ami-0557a15b87f6559cf"
+  instance_type = "t2.micro"
+  tags = {
+    "name" = "Server_one"
+  }
+   
+ }
+
 
 resource "aws_security_group" "SG1" {
-  name = "Security grup para load balancer"
+  name = "Security_grup para load balancer"
 
   ingress {
 
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
     description = "acceso al puerto 8080"
     from_port = "8080"
     to_port = "8080"
