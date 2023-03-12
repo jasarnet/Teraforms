0cf2487c752cad26662adb7c979de4e3df6e6017
jasar <juanantonio.net@gmail.com>
2023-03-11 23:12:44 -0600

    Dos servers, funcional

--- main.tf
+++ main.tf
@@ -1,15 +1,57 @@
 provider "aws" {
   region = "us-east-1"
-  
+
+}  
+
+data "aws_subnet" "az_a" {
+    availability_zone = "us-east-1a"
 }
+
+
+
+
+data "aws_subnet" "az_b" {
+    availability_zone = "us-east-1b"
+}
+
+
+
+
  resource "aws_instance" "Server1" {
-  ami = "ami-0557a15b87f6559cf"
-  instance_type = "t2.micro"
-  tags = {
-    "name" = "Server_one"
-  }
-   
+  ami                    = "ami-0557a15b87f6559cf"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.SG1.id]
+  subnet_id              =  data.aws_subnet.az_a.id
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hola Server1!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+    tags = {
+      "Name" = "ServerOne"
+    }
+       
  }
+
+
+resource "aws_instance" "Server2" {
+  ami                    = "ami-0557a15b87f6559cf"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.SG1.id]
+  subnet_id              =  data.aws_subnet.az_b.id
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hola server2!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+    tags = {
+      "Name" = "ServerTwo"
+    }
+       
+ }
+
+
 
 
 resource "aws_security_group" "SG1" {
